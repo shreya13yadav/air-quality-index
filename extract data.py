e90e1ae46aee89a2768f7ec22a2993957ce1e141
shreya13yadav <palaky1310@gmail.com>
2025-07-19 13:33:29 +0530

    Create extract data.py

---
+++ extract data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Data is available in Kaggle as /kaggle/input/air-quality-data-in-india/city_day.csv
+df = pd.read_csv('/kaggle/input/air-quality-data-in-india/city_day.csv')
+print("Data extracted from Kaggle")
